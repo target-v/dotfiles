256009a5eadc1270c1a699669ea3cc8652f5bbdb
Jason Pleau <jason@numerique.ca>
2016-05-06 11:46:36 -0400

    dont enable git for non ext2,3,4

--- zsh/.zsh/functions.zsh
+++ zsh/.zsh/functions.zsh
@@ -41,9 +41,12 @@ function take() {
 
 # next two functions taken from oh-my-zsh
 function git_prompt_info() {
-	ref=$(command git symbolic-ref HEAD 2> /dev/null) || \
-	ref=$(command git rev-parse --short HEAD 2> /dev/null) || return 0
-	echo "$ZSH_THEME_GIT_PROMPT_PREFIX${ref#refs/heads/}$(parse_git_dirty)$ZSH_THEME_GIT_PROMPT_SUFFIX"
+	testdir=`stat -f -L -c %T $(pwd)`
+	if [[ "$testdir" == "ext2/ext3" ]]; then
+		ref=$(command git symbolic-ref HEAD 2> /dev/null) || \
+		ref=$(command git rev-parse --short HEAD 2> /dev/null) || return 0
+		echo "$ZSH_THEME_GIT_PROMPT_PREFIX${ref#refs/heads/}$(parse_git_dirty)$ZSH_THEME_GIT_PROMPT_SUFFIX"
+	fi
 }
 
 function parse_git_dirty() {
